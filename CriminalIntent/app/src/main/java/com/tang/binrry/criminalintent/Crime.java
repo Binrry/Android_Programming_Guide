245c967d78bbc7f09f7b11f6ff997bd3bf411774
Binrry <184471715@qq.com>
2019-09-09 22:51:05 +0800

    Unit Seven

---
+++ CriminalIntent/app/src/main/java/com/tang/binrry/criminalintent/Crime.java
@@ -0,0 +1,49 @@
+package com.tang.binrry.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+//Unit Seven Begin
+public class Crime
+{
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean solved) {
+        mSolved = solved;
+    }
+}
